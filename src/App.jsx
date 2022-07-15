2230a8d6a358ca7e7e6752dd933c73ae151dae7c
kashifkhan0336 <kashifkhan0336@gmail.com>
2022-07-15 08:34:31 +0500

    as

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <h1 className="text-4xl">Hello world!</h1>
+    </div>
+  );
+}
+
+export default App;
